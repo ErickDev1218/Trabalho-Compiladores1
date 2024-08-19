9e8d608a90d48b0f3e8158a79d87da8cde9d30db
GusGurgel <gurgelg15@gmail.com>
2024-08-19 19:34:50 -0300

    TODO functions of thompsonConstruction.ts

---
+++ src/thompsonConstruction.ts
@@ -0,0 +1,13 @@
+import Automata from "./Automata";
+
+function AutomataSum(A1 : Automata, A2 : Automata) : Automata {
+
+}
+
+function AutomataCancat(A1 : Automata, A2 : Automata) :Automata {
+
+}
+
+function AutomataKleene(A1 : Automata) : Automata {
+
+}
